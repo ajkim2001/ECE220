daee67d0d3b0dfe9bae8bc1b33bcc12eeb887533
Alex Kim <alexk4@illinois.edu>
2021-03-13 00:28:56 -0600

    completed mp5

--- mp5/prog5.c
+++ mp5/prog5.c
@@ -11,7 +11,21 @@
  * in this file to help you get started.
  */
 
-
+/* 
+Alex Kim netid:alexk4 3/12/2021
+In this program I had to implement 3 functions: set_seed, start_game, and make_guess.
+For set_seed, I just had to print an error message and return 0 if the if statement came true.
+Otherwise, I just called srand and returned 1.
+For start_game, I just had to use a for loop to create 4 random numbers that I used to select
+the names in a pool randomly. To do this, I used an array and strcpy to copy it to another array.
+Then I simply initialized max_score and guess_number.
+Finally, I had to write make_guess. This function required me to use sscanf to move names from 
+the guess string the user inputted to my own arrays where I could check if the guesses were valid
+and how they compared to the solutions. The comparison was done using for loops that made use of
+strcmp to check through each element. I then either output and error message with return 0 or
+output the user's score and result of the guess until all 10 guesses were used or it was guessed
+perfectly correct.
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -88,8 +102,14 @@ int set_seed (const char seed_str[]) {
     char post[2];
     if (sscanf (seed_str, "%d%1s", &seed, post) != 1) {
       // your code here
+      printf("set_seed: invalid seed\n"); /* prints error message and returns 0 if sscanf does not find 1 integer */
+      return 0;
     }
     // your code here
+    else { /* if 1 integer was found, call srand with it and return 1 */
+    srand(seed);
+    return 1;
+    }
 }
 
 
@@ -107,7 +127,14 @@ int set_seed (const char seed_str[]) {
  * SIDE EFFECTS: records the solution in the static solution variables for use by make_guess, set guess_number
  */
 void start_game () {
-    //your code here
+    // your code here
+    int i;
+    for (i = 0; i < 4; i++){ 
+        int random = rand() % 8; /* Use rand and % 8 to find a random position in pool */
+        strcpy (solutions[i], pool[random]); /* Use the variable in the for loop and strcpy to copy the randomly chose name to solutions */
+    }
+    max_score = -1; /* initialize variables */
+    guess_number = 1;
 }
 
 /*
@@ -131,6 +158,75 @@ void start_game () {
  */
 int make_guess (const char guess_str[]) {
   // your code here
+  int current; /* current score */
+  int perfect_match = 0; /* how many perfect matches found */
+  int misplaced_match = 0; /* how many misplaced matches found */
+  char guess1[10], guess2[10], guess3[10], guess4[10]; /* guesses from guess_str copied over in sscanf */
+  char extra[1]; /* extra in guess_str */
+  int guess_match[4] = {0, 0, 0, 0,}; /* which guesses were matched */
+  int solution_match[4] = {0, 0, 0, 0,}; /* which solutions were matched */
+  int ret_val; /* return value of sscanf */
+
+  ret_val = sscanf(guess_str, "%s%s%s%s%1s", guess1,guess2,guess3,guess4,extra); /* Use sscanf to read data and store from guess_str to my arrays */
+  if (ret_val == 4) /* keep running if sscanf found exactly 4 items */
+  {
+    char* guesses[4] = {guess1, guess2, guess3, guess4}; /* array of guesses */
+    for (int a = 0; a < 4; a++) 
+    {
+        if (is_valid(guesses[a]) == 0) /* checks if each guess is a valid guess, if not prints error and returns 0 */
+        {
+            printf("make_guess: invalid guess\n");
+            return 0;
+        }
+    }
+    for (int b = 0; b < 4; b++) /* compares guesses to solutions to see if there is a perfect match in any of the same spots */
+    {
+        if (strcmp(guesses[b], solutions[b]) == 0)
+        {
+            solution_match[b] = 1;
+            guess_match[b] = 1;
+            perfect_match ++;
+        }
+    }
+    for (int c = 0; c < 4; c++)
+    {
+        if (guess_match[c] == 1) /* checks if the guess was already matched */
+        {
+            continue;
+        }
+        for (int d = 0; d < 4; d++) 
+        {
+            if (solution_match[d] == 1) /* checks if the solution was already matched */
+            {
+                continue;
+            }
+            if (strcmp(guesses[c], solutions[d]) == 0) /* compares guesses to solutions to see if there is any misplaced matches in different spots */
+            {
+                solution_match[d] = 1;
+                guess_match[c] = 1;
+                misplaced_match ++;  
+            }
+        }
+    }
+    current  = (perfect_match*1000) + (misplaced_match*100); /* calculated current score using number of perfect and misplaced matches */
+    if (current > max_score){ /* If the current score is larger than the max score, replace max score with current */
+        max_score = current;
+    }
+    if (perfect_match == 4){ /* If 4 perfect matches have been found, return 2 */
+        return 2;
+    }
+    else { /* Otherwise print out the current data of matches and score and increment the number of guesses made and return 1 */
+    printf("With guess %d, you got %d perfect matches and %d misplaced matches.\n", guess_number, perfect_match, misplaced_match);
+    printf("Your score is %d and cur max score is %d.\n", current, max_score);
+    guess_number++;
+    return 1;
+    }
+  }
+  else { /* If 4 items not found, print an error and return 0 */
+    printf("make_guess: invalid guess\n");
+    return 0;
+  
+  }
 }
 
 
